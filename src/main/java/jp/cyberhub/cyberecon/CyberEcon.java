ab4a5ae9912ee99d1f86ba2d7597bc6a9f53e9d0
Haru-93 <hal-ishikawa2046@outlook.jp>
2024-12-14 12:45:25 +0900

    Initial commit

---
+++ src/main/java/jp/cyberhub/cyberecon/CyberEcon.java
@@ -0,0 +1,17 @@
+package jp.cyberhub.cyberecon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CyberEcon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
